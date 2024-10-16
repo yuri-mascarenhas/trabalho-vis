b9ed1e565b000a9fdc47965a354cd3ae0881bab5
yurism <mascarenhasyuri@hotmail.com>
2024-10-15 21:27:53 -0300

    [Fix] Legend organization

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function createChart({
   description,
 }) {
   // Set the dimensions and margins of the chart
-  const margin = { top: 60, right: 30, bottom: 50, left: 70 };
+  const margin = { top: 60, right: 30, bottom: 120, left: 70 };
   const width = 800 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
 
@@ -68,8 +68,8 @@ function createChart({
     .append("g")
     .call(d3.axisLeft(yScale))
     .append("text")
-    .attr("x", -40)
-    .attr("y", -10)
+    .attr("x", 0)
+    .attr("y", -40)
     .attr("fill", "black")
     .attr("class", "axis-label")
     .style("text-anchor", "end")
@@ -110,7 +110,7 @@ function createChart({
   if (colors.length && description.length) {
     const legend = svg
       .append("g")
-      .attr("transform", `translate(${width - 100}, 20)`);
+      .attr("transform", `translate(0, ${height + 40})`);
 
     colors.forEach((color, index) => {
       legend
