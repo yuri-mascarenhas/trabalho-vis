77eb0629b1d1cff723b853e1e7d3e9fd5c336af4
yurism <mascarenhasyuri@hotmail.com>
2024-11-07 19:22:27 -0300

    [Refac] File structure and organization

---
+++ classes/BarChart.js
@@ -0,0 +1,7 @@
+class BarChart {
+  constructor(selector, config, data) {
+    this.selector = selector;
+    this.config = config;
+    this.data = data;
+  }
+}
---
+++ classes/Config.js
@@ -0,0 +1,7 @@
+class Config {
+  constructor(width, height, margin) {
+    this.width = width;
+    this.height = height;
+    this.margin = margin;
+  }
+}
