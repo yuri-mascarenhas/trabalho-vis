6b72d01fde7ac76ee57ecfa2d13d8ee3830940ed
yurism <mascarenhasyuri@hotmail.com>
2024-11-19 18:02:09 -0300

    [Fix] Graph creation

--- classes/BarChart.js
+++ classes/BarChart.js
@@ -3,17 +3,20 @@ class BarChart {
     this.selector = selector;
     this.config = config;
     this.data = data;
+    this.width = config.width;
+    this.height = config.height;
     this.svg = null;
     this.initChart();
   }
 
   initChart() {
+    // Convert data into array of objects
     this.aggregatedData = Array.from(this.data, ([category, sales]) => ({
       category,
       sales,
     }));
-    console.log("Aggregated Data:", this.aggregatedData);
 
+    // SVG creation
     this.svg = d3
       .select(this.selector)
       .append("svg")
@@ -28,47 +31,50 @@ class BarChart {
       .append("g")
       .attr(
         "transform",
-        `translate(${this.config.margin.left}, ${this.config.margin.top})`
+        `translate(${this.config.margin.left},${this.config.margin.top})`
       );
 
+    // Scales
     this.x = d3
       .scaleBand()
       .domain(this.aggregatedData.map((d) => d.category))
       .range([0, this.width])
       .padding(0.2);
+
     this.y = d3
       .scaleLinear()
       .domain([0, d3.max(this.aggregatedData, (d) => d.sales)])
       .range([this.height, 0]);
-    console.log("X scale domain:", this.x.domain()); // Debug: Check x scale domain
-    console.log("Y scale domain:", this.y.domain()); // Debug: Check y scale domain
-    // X axis
+    // Draw x-axis
     this.svg
       .append("g")
       .attr("transform", `translate(0,${this.height})`)
       .call(d3.axisBottom(this.x));
-    // Y axis
+
+    // Draw y-axis
     this.svg.append("g").call(d3.axisLeft(this.y));
   }
 
   render() {
+    // Render bars using aggregated data
     this.svg
       .selectAll("rect")
       .data(this.aggregatedData)
       .enter()
       .append("rect")
       .attr("x", (d) => {
-        const xVal = this.x(d.category); // Check the value of x(d.category)
-        console.log("X value:", xVal); // Debug: Check the calculated x value
+        const xVal = this.x(d.category);
         return xVal;
       })
       .attr("y", (d) => {
-        const yVal = this.y(d.sales); // Check the value of y(d.sales)
-        console.log("Y value:", yVal); // Debug: Check the calculated y value
+        const yVal = this.y(d.sales);
         return yVal;
       })
-      .attr("width", this.x.bandwidth())
-      .attr("height", (d) => this.height - this.y(d.sales))
-      .attr("fill", this.config.colors.bar);
+      .attr("width", this.x.bandwidth()) // Ensure bandwidth is valid
+      .attr("height", (d) => {
+        const heightVal = this.height - this.y(d.sales);
+        return heightVal;
+      })
+      .attr("fill", this.config.colors.bar); // Use config for colors
   }
 }
